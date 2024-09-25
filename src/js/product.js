1b386ce380b35d78efa52024509a2b191ae5e541
vern wolfley <vern.wolfley@gmail.com>
2024-09-24 17:58:16 -0700

    update product pages html

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,10 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParams} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+
+const productId = getParams('product');
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   const productList = getLocalStorage("so-cart") || []
