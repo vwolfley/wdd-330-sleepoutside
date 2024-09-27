8cba8899373538abc6c237bc54af851971a3cab2
eworsham <eworsham64@gmail.com>
2024-09-26 21:01:00 -0400

    Added X button and styled it

--- src/js/cart.js
+++ src/js/cart.js
@@ -21,6 +21,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="cart-card__remove">X</button>
 </li>`;
 
   return newItem;
