4b2f17fc623c71fb843b93eecc0b02d5a0935715
vern wolfley <vern.wolfley@gmail.com>
2024-10-04 16:19:44 -0700

    update cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,6 +8,26 @@ loadHeaderFooter();
 
 const cartItems = getLocalStorage("so-cart") || [];
 
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Images.PrimaryMedium}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="cart-card__remove" data-id="${item.Id}">X</button>
+</li>`;
+
+  return newItem;
+}
+
 function renderCartContents() {
   // If there are no items, we can stop here or show a message
   if (cartItems.length === 0) {
@@ -42,26 +62,6 @@ function removeCartItem(event) {
   setLocalStorage("so-cart", cartItemRemoved);
 
   renderCartContents();
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Images.PrimaryMedium}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button class="cart-card__remove" data-id="${item.Id}">X</button>
-</li>`;
-
-  return newItem;
 }
 
 function cartSubtotal(items) {
