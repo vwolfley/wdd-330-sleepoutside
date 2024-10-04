8593edce331b713ef69483b7af97f109c28466f0
eworsham <eworsham64@gmail.com>
2024-10-04 15:45:38 -0400

    Fixed cart image

--- src/js/cart.js
+++ src/js/cart.js
@@ -48,7 +48,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimarySmall}"
       alt="${item.Name}"
     />
   </a>
