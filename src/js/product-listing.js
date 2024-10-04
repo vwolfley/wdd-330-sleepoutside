ddee5619c85cf75cfde4d3b57b377d4d2dbcb8c4
eworsham <eworsham64@gmail.com>
2024-10-04 14:27:16 -0400

    moved product listing js code to new file

---
+++ src/js/product-listing.js
@@ -0,0 +1,12 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+// Create an instance of ProductData
+const productData = new ProductData("tents");
+
+// Define the list element
+const listElement = document.querySelector(".product-list");
+
+// Create an instance of ProductList
+const productListing = new ProductList("tents", productData, listElement);
+productListing.init();
