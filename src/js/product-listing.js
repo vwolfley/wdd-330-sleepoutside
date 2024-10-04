4ce564615eddd2374cb9e454a16425bd2d4ba731
eworsham <eworsham64@gmail.com>
2024-10-04 15:06:57 -0400

    Fixed up product listing page

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,12 +1,18 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { getParams, loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter()
+
+// Get category type from url
+const category = getParams('category');
 
 // Create an instance of ProductData
-const productData = new ProductData("tents");
+const productData = new ProductData();
 
 // Define the list element
 const listElement = document.querySelector(".product-list");
 
 // Create an instance of ProductList
-const productListing = new ProductList("tents", productData, listElement);
+const productListing = new ProductList(category, productData, listElement);
 productListing.init();
