2d1999dfe6fd4f79130c217845ae799fa27df2d0
eworsham <eworsham64@gmail.com>
2024-10-04 14:27:59 -0400

    Updated product data mjs to pull from an api instead of json file

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,5 @@
+const baseURL = import.meta.env.VITE_SERVER_URL
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -7,14 +9,13 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
+  constructor() {
+    
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`)
+    const data = await convertToJson(response)
+    return data
   }
   async findProductById(id) {
     const products = await this.getData();
